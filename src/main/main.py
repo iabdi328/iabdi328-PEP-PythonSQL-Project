0c82680f8101c86ca7981af9afcccc98ad854d22
iabdi328 <iabdi328@github.com>
2026-07-14 04:31:07 +0000

    Test run: 0/5 passed, 5 failed, 0 skipped - 2026-07-14T04:31:07.815Z

--- src/main/main.py
+++ src/main/main.py
@@ -49,6 +49,12 @@ def main():
 # This function will load the users.csv file into the users table, discarding any records with incomplete data
 def load_and_clean_users(file_path):
 
+    with open(file_path, "r", newline="") as l:
+        for line in l:
+            print(line)
+
+
+
     print("TODO: load_users")
 
 
